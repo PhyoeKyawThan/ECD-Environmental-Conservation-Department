a0328a8c62818156f79ca8a8d3da3afb3a6f0ab3
PhyoeKyawThan <kyawthan.phyoe9@gmail.com>
2025-05-24 13:16:26 +0630

    add: prepare search func and media model search

--- models/mediaModel.php
+++ models/mediaModel.php
@@ -1,18 +1,28 @@
 <?php
 
-require_once __DIR__.'/Connection.php';
-require_once __DIR__.'/prepare_funcs.php';
-
-class MediaModel extends Connection{
+require_once __DIR__ . '/Connection.php';
+require_once __DIR__ . '/prepare_funcs.php';
+/**
+ * Model for media table basic crud to advenced search
+ * 
+ */
+class MediaModel extends Connection
+{
     private $table_name = "media";
     public $id;
     public $title;
     public $description;
     public $images;
     public $uploaded_at;
+    public $extra; // for used in search column or somethng, format ["search_term" => "string", "columns" => ["colum1", colum2"]]
 
-    public function add(){
-        try{
+    /**
+     * Adding new media data to database 
+     * @return bool insertion success or not
+     */
+    public function add()
+    {
+        try {
             $query = prepare_insert_query(
                 parent::$connection,
                 $this->table_name,
@@ -24,13 +34,14 @@ class MediaModel extends Connection{
                 )
             );
             return $query->execute();
-        }catch(Exception $e){
-            die("Error in adding media: ".$e);
+        } catch (Exception $e) {
+            die("Error in adding media: " . $e);
         }
     }
 
-    public function get(){
-        try{
+    public function get()
+    {
+        try {
             $query = prepare_select(
                 parent::$connection,
                 $this->table_name,
@@ -41,49 +52,70 @@ class MediaModel extends Connection{
             $result = $result->fetch_assoc();
 
             return $result;
-        }catch(Exception $e){
+        } catch (Exception $e) {
             die("Error occur while fetching media data");
         }
     }
 
-    public function get_all(){
-        try{
+    public function get_all()
+    {
+        try {
             $query = prepare_select_all(
                 parent::$connection,
                 $this->table_name,
             );
             $result = $query->fetch_all(MYSQLI_ASSOC);
             return $result;
-        }catch(Exception $e){
+        } catch (Exception $e) {
             die("Error occur while fetching media data");
         }
     }
 
-    public function update(){
-        try{
+    public function search()
+    {
+        // format ["search_term" => "string", "columns" => ["colum1", colum2"]]
+        try {
+            $query = prepare_search_query(
+                parent::$connection,
+                $this->table_name,
+                $this->extra["columns"],
+                $this->extra["search_term"]
+            );
+            $query->execute();
+            $result = $query->get_result();
+            return $result->fetch_all(MYSQLI_ASSOC);
+        } catch (Exception $e) {
+            die("Error while searching");
+        }
+    }
+
+    public function update()
+    {
+        try {
 
             $datas = array(
                 "title" => $this->title,
                 "description" => $this->description,
             );
-            if(isset($this->images)){
+            if (isset($this->images)) {
                 $datas["images"] = $this->images;
             }
             $datas["id"] = $this->id;
             $query = prepare_update_query(
                 parent::$connection,
                 $this->table_name,
-                $datas    
+                $datas
             );
 
             return $query->execute();
-        }catch(Exception $e){
-            die("Error in updating media: ".$e);
+        } catch (Exception $e) {
+            die("Error in updating media: " . $e);
         }
     }
 
-    public function delete(){
-        try{
+    public function delete()
+    {
+        try {
             $query = prepare_delete_query(
                 parent::$connection,
                 $this->table_name,
@@ -91,16 +123,17 @@ class MediaModel extends Connection{
             );
 
             return $query->execute();
-        }catch(Exception $e){
-            die("Error in deleting media: ".$e);
+        } catch (Exception $e) {
+            die("Error in deleting media: " . $e);
         }
     }
-    private function convert_image(){
+    private function convert_image()
+    {
 
     }
 }
 
-// $media = new MediaModel();
+$media = new MediaModel();
 // $media->id = 2;
 // // print_r($media->get_all());
 // $media->title = "Something";
@@ -108,4 +141,4 @@ class MediaModel extends Connection{
 // $media->images = "Image1, Image2 up. Image3";
 // $media->update();
 
-// $media->add();
+$media->add();
--- models/prepare_funcs.php
+++ models/prepare_funcs.php
@@ -1,14 +1,35 @@
 <?php
 require_once __DIR__ . '/helper_funcs.php';
+
+/**
+ * Prepare query for inserting operation.
+ *
+ * @param mysqli                        $connection The MySQLi connection object.
+ * @param string                        $table_name The name of the table to insert into.
+ * @param array<string,mixed>           $datas      Associative array of column => value pairs.
+ * @param string                        $types      String of types for bind_param (e.g., 'ssi').
+ * @return mysqli_stmt                              The prepared mysqli statement.
+ */
 function prepare_insert_query($connection, $table_name, $datas, $types = "sss"): mysqli_stmt
 {
     $columns = implode(", ", array_keys($datas));
     $placeholders = implode(", ", array_fill(0, count($datas), '?'));
     $query_str = "INSERT INTO $table_name($columns) VALUES($placeholders)";
     $query = $connection->prepare($query_str);
-    $query->bind_param($types, ...array_values($datas));
+    $values = array_values($datas);
+    $query->bind_param($types, ...$values);
     return $query;
 }
+
+/**
+ * Prepares a parameterized UPDATE query.
+ *
+ * @param mysqli               $connection The MySQLi connection object.
+ * @param string               $table_name The name of the table to update.
+ * @param array<string, mixed> $datas      Associative array of column => value pairs. Must include 'id' key.
+ * @param string               $types      A string of bind_param types (e.g., 'ssi').
+ * @return mysqli_stmt                     The prepared mysqli statement.
+ */
 
 function prepare_update_query($connection, $table_name, $datas, $types = "sssi"): mysqli_stmt
 {
@@ -29,7 +50,14 @@ function prepare_update_query($connection, $table_name, $datas, $types = "sssi")
 
     return $query;
 }
-
+/**
+ * Prepares a DELETE query by ID.
+ *
+ * @param mysqli  $connection The MySQLi connection object.
+ * @param string  $table_name The name of the table to delete from.
+ * @param int     $id         The ID of the row to delete.
+ * @return mysqli_stmt        The prepared mysqli statement.
+ */
 function prepare_delete_query($connection, $table_name, $id): mysqli_stmt
 {
     $query_str = "DELETE FROM $table_name WHERE id = ?";
@@ -38,7 +66,14 @@ function prepare_delete_query($connection, $table_name, $id): mysqli_stmt
 
     return $query;
 }
-
+/**
+ * Prepares a SELECT query by ID.
+ *
+ * @param mysqli  $connection The MySQLi connection object.
+ * @param string  $table_name The name of the table to select from.
+ * @param int     $id         The ID of the row to select.
+ * @return mysqli_stmt        The prepared mysqli statement.
+ */
 function prepare_select($connection, $table_name, $id): mysqli_stmt
 {
     $query_str = "SELECT * FROM $table_name WHERE id = ?";
@@ -47,7 +82,44 @@ function prepare_select($connection, $table_name, $id): mysqli_stmt
 
     return $query;
 }
+/**
+ * Prepares a search query with multiple columns using LIKE conditions.
+ *
+ * @param mysqli        $connection    The MySQLi connection object.
+ * @param string        $table_name    The name of the table to search.
+ * @param string[]      $columns_arry  Array of column names to search in.
+ * @param string        $search_term   The search keyword.
+ * @throws \Exception                 Throws exception if prepare fails.
+ * @return mysqli_stmt|bool            Prepared statement on success, false on failure.
+ */
+function prepare_search_query(mysqli $connection, string $table_name, array $columns_arry, string $search_term): mysqli_stmt
+{
+    // Build WHERE clause like: column1 LIKE ? OR column2 LIKE ? ...
+    $where_clauses = array_map(fn($col) => "$col LIKE ?", $columns_arry);
+    $where_sql = implode(" OR ", $where_clauses);
 
+    $query_str = "SELECT * FROM $table_name WHERE $where_sql ORDER BY uploaded_at DESC";
+
+    $stmt = $connection->prepare($query_str);
+    if (!$stmt) {
+        throw new Exception("Prepare failed: " . $connection->error);
+    }
+    // Repeat the search term for each key
+    $search_values = array_fill(0, count($columns_arry), "%$search_term%");
+    $types = str_repeat('s', count($columns_arry)); // All strings
+
+    $stmt->bind_param($types, ...$search_values);
+
+    return $stmt;
+}
+
+/**
+ * Executes a SELECT * query on the specified table.
+ *
+ * @param mysqli $connection The MySQLi connection object.
+ * @param string $table_name The name of the table to select from.
+ * @return mysqli_result     The result set from the query.
+ */
 function prepare_select_all($connection, $table_name): mysqli_result
 {
     $query_str = "SELECT * FROM $table_name ORDER BY id DESC";
